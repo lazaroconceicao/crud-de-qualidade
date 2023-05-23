70f121cb809bcd88d572ab9c831fd30543506847
lazaroconceicao <joselazaroc@gmail.com>
2023-05-23 10:44:57 -0300

    Paths relativos

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { GlobalStyles } from "../src/theme/GlobalStyle";
+import { GlobalStyles } from "../src/ui/theme/GlobalStyle";
 
 // const bg = "https://raw.githubusercontent.com/devsoutinho/aulas-crudcomqualidade.io/2f69fc697f76c1ed6385480b649185a0d258da5b/public/bg.jpeg";
 
@@ -9,7 +9,7 @@ const bg = "/bg.jpeg"; //inside public folder
 export default function Page() {
     return (
       <main>
-        <GlobalStyles themeName="coolGrey"/>
+        <GlobalStyles themeName="indigo"/>
          <header
            style={{
               backgroundImage: `url('${bg}')`,
